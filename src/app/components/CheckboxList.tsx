8f5dc65e2a3dd39902110dd3ab24fef58b4376e1
Ishika Mishra <mishraishika209@gmail.com>
2024-12-04 19:36:29 +0530

    Fix: fixes sidebar for mobile

--- src/app/components/CheckboxList.tsx
+++ src/app/components/CheckboxList.tsx
@@ -58,6 +58,7 @@ export default function CheckboxList({ list }) {
               </IconButton>
             }
             disablePadding
+            className={checked.includes(value) ? "item-selected" : ""}
           >
             <ListItemButton
               role={undefined}
